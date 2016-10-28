0760a57737ae6b27a1cbde5728bd3e948dbe45f1
Jensen Reitz <JLReitz@live.com>
2016-10-28 10:02:41 -0700

    Update WSU_Aerospace_Glider.ino

--- WSU_Aerospace_Glider/WSU_Aerospace_Glider.ino
+++ WSU_Aerospace_Glider/WSU_Aerospace_Glider.ino
@@ -4,7 +4,7 @@
  * 
  * A program implemented to autonomously guide a glider from its release from the WSU Aerospace Club's rocket in-air back to the launch site.
  * 
- * Designed and written by Jensen Reitz
+ * Designed and written by Jensen Reitz of WSU Robotics
  * 
  * - Utilizing Tinyduino and various TinySheilds, including the TinySheildGPS, by TinyCircuits.com
  * - Special thanks to Ben Rose of TinyCurcuits for his help in converting the GNRMC strings output by the TinySheild GPS into GPRMC strings that are usable by various tracking 
